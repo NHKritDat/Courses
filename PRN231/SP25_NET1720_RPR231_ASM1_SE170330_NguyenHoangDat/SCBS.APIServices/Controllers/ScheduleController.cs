382db19c7a02ab2c61b4e80ea79a1f8d63ad0553
Krit Dat <113153168+NHKritDat@users.noreply.github.com>
2025-01-21 16:37:30 +0700

    PRN231

--- PRN231/SP25_NET1720_RPR231_ASM1_SE170330_NguyenHoangDat/SCBS.APIServices/Controllers/ScheduleController.cs
+++ PRN231/SP25_NET1720_RPR231_ASM1_SE170330_NguyenHoangDat/SCBS.APIServices/Controllers/ScheduleController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SCBS.Repositories.Models;
+using SCBS.Services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -6,38 +9,42 @@ namespace SCBS.APIServices.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ScheduleController : ControllerBase
     {
+        private readonly IScheduleService _scheduleService;
+        public ScheduleController(IScheduleService scheduleService) => _scheduleService = scheduleService;
+
         // GET: api/<ScheduleController>
         [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        [Authorize(Roles = "1,2")]
+        public async Task<IEnumerable<Schedule>> Get() => await _scheduleService.GetAllAsync();
 
         // GET api/<ScheduleController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+        [Authorize(Roles = "1,2")]
+        public async Task<Schedule?> Get(Guid id) => await _scheduleService.GetByIdAsync(id);
+
+        [HttpGet("{workDate}/{status}")]
+        [Authorize(Roles = "1,2")]
+        public async Task<IEnumerable<Schedule>> Get(DateTime workDate, string status) => await _scheduleService.Search(workDate, status);
 
         // POST api/<ScheduleController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        [Authorize(Roles = "1,2")]
+        public async Task<int> Post(Schedule schedule)
         {
+            return await _scheduleService.Create(schedule);
         }
 
         // PUT api/<ScheduleController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        [Authorize(Roles = "1,2")]
+        public async Task<int> Put(Schedule schedule) => await _scheduleService.Update(schedule);
 
         // DELETE api/<ScheduleController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        [Authorize(Roles = "1")]
+        public async Task<bool> Delete(Guid id) => await _scheduleService.Delete(id);
     }
 }
--- PRN231/SP25_NET1720_RPR231_ASM1_SE170330_NguyenHoangDat/SCBS.APIServices/Controllers/UserController.cs
+++ PRN231/SP25_NET1720_RPR231_ASM1_SE170330_NguyenHoangDat/SCBS.APIServices/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SCBS.Repositories.Models;
 using SCBS.Services;
 
@@ -8,12 +9,14 @@ namespace SCBS.APIServices.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
         public UserController(IUserService userService) => _userService = userService;
         // GET: api/<UserController>
         [HttpGet]
+        [Authorize(Roles = "1,2")]
         public async Task<IEnumerable<User>> Get()
         {
             return await _userService.GetAll();
@@ -21,12 +24,14 @@ namespace SCBS.APIServices.Controllers
 
         // GET api/<UserController>/5
         [HttpGet("{id}")]
+        [Authorize(Roles = "1,2")]
         public async Task<User?> Get(Guid id)
         {
             return await _userService.GetById(id);
         }
 
         [HttpGet("{username?}/{email?}/{fullName?}")]
+        [Authorize(Roles = "1,2")]
         public async Task<IEnumerable<User?>> Get(string username, string email, string fullName)
         {
             return await _userService.Search(username, email, fullName);
@@ -34,6 +39,7 @@ namespace SCBS.APIServices.Controllers
 
         // POST api/<UserController>
         [HttpPost]
+        [Authorize(Roles = "1,2")]
         public async Task<int> Post(User user)
         {
             return await _userService.Create(user);
@@ -41,6 +47,7 @@ namespace SCBS.APIServices.Controllers
 
         // PUT api/<UserController>/5
         [HttpPut]
+        [Authorize(Roles = "1,2")]
         public async Task<int> Put(User user)
         {
             return await _userService.Update(user);
@@ -48,6 +55,7 @@ namespace SCBS.APIServices.Controllers
 
         // DELETE api/<UserController>/5
         [HttpDelete("{id}")]
+        [Authorize(Roles = "1")]
         public async Task<bool> Delete(Guid id)
         {
             return await _userService.Delete(id);
