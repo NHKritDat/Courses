7425107257f37880b45a1645d608270f0a4959a4
Krit Dat <113153168+NHKritDat@users.noreply.github.com>
2025-02-18 18:41:51 +0700

    PRN231

---
+++ PRN231/SP25_NET1720_RPR231_ASM2_SE170330_NguyenHoangDat/SCBS.GrpcService/Services/ScheduleService.cs
@@ -0,0 +1,35 @@
+﻿using Grpc.Core;
+
+namespace SCBS.GrpcService.Services
+{
+    public class ScheduleService : ScheduleProto.ScheduleProtoBase
+    {
+        private readonly ILogger<ScheduleService> _logger;
+        public ScheduleService(ILogger<ScheduleService> logger) => _logger = logger;
+        public override Task<GetAllReply> GetAll(GetAllRequest request, ServerCallContext context)
+        {
+            var reply = new GetAllReply();
+            ScheduleItem item1 = new ScheduleItem
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserId = Guid.NewGuid().ToString(),
+                WorkDate = DateTime.Now.ToString(),
+                Status = "Offline",
+                CreatedAt = DateTime.Now.ToString(),
+                UpdatedAt = DateTime.Now.ToString()
+            };
+            reply.Items.Add(item1);
+            ScheduleItem item2 = new ScheduleItem
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserId = Guid.NewGuid().ToString(),
+                WorkDate = DateTime.Now.ToString(),
+                Status = "Work",
+                CreatedAt = DateTime.Now.ToString(),
+                UpdatedAt = DateTime.Now.ToString()
+            };
+            reply.Items.Add(item2);
+            return Task.FromResult(reply);
+        }
+    }
+}
